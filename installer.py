5e1061aae11355cc392e5fc7adfa5bbb1b4bbf23
hunter32me <57907467+hunter32me@users.noreply.github.com>
2023-07-06 10:32:12 -0500

    Type in installer and ingest pipleine

--- installer.py
+++ installer.py
@@ -221,7 +221,7 @@ def datastreams(session, baseURI, logstash,updateTemplates):
         exportToElastic(session, baseURI, index, filename, "/_index_template/", retry=4)
     if not logstash:
         exportToElastic(session, baseURI, ingest, "corelight-ds-component_template-use_ingest_pipeline-settings", "/_component_template/", retry=4)
-        exportToElastic(session, baseURI, ingest, "corelight-ds-index_template-main_logs_use_ingest_pipeline", "/_index_template/", retry=4)
+        exportToElastic(session, baseURI, ingest, "corelight-ds-index_template-main_logs", "/_index_template/", retry=4)
         exportToElastic(session, baseURI, ingest, "corelight-ds-index_template-metrics_and_stats_use_ingest_pipeline", "/_index_template/", retry=4)
 
 def component( session, baseURI, logstash, updateTemplates ):
@@ -242,12 +242,8 @@ def component( session, baseURI, logstash, updateTemplates ):
         exportToElastic(session, baseURI, index, filename, "/_index_template/", retry=4)
     if not logstash:
         exportToElastic(session, baseURI, ingest, "corelight-non-ds-component_template-use_ingest_pipeline-settings", "/_component_template/", retry=4)
-        fileList=os.listdir(ingest)
-        for filename in fileList:
-            if filename == "corelight-non-ds-component_template-use_ingest_pipeline-settings":
-                exportToElastic(session, baseURI, ingest, filename, "/_component_template/", retry=4)
-            else:
-                exportToElastic(session, baseURI, ingest, filename, "/_index_template/", retry=4)
+        exportToElastic(session, baseURI, ingest, "corelight-non-ds-index_template-main_logs_use_ingest_pipeline", "/_index_template/", retry=4)
+        exportToElastic(session, baseURI, ingest, "corelight-non-ds-index_template-metrics_and_stats_use_ingest_pipeline", "/_index_template/", retry=4)
 
 def index(session, baseURI, logstash,updateTemplates):
     source = "./templates-component/templates-legacy/"
